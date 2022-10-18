5adcfec29c349667d42284b4c0c822f848f81201
Usuario <Usuario@CPD01-M07>
2022-10-18 15:10:26 -0300

    Adicionar arquivos de projeto.

---
+++ AppConselhos/AppConselhos/Model/Conselho.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConselhos.Model
+{
+    internal class Conselho
+    {
+    }
+}
---
+++ AppConselhos/AppConselhos/Services/DataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConselhos.Services
+{
+    internal class DataService
+    {
+    }
+}
