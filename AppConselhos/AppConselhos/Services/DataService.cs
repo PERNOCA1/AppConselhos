f1f49b9ccf5d5f5b1516760e7a52ddc7dc971c4b
Gabriel <gabluiz18disp@gmail.com>
2022-11-21 19:50:10 -0300

    finish 1/2

--- AppConselhos/AppConselhos/Services/DataService.cs
+++ AppConselhos/AppConselhos/Services/DataService.cs
@@ -6,17 +6,18 @@ using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AppConselhos.Model;
+using Microsoft.CSharp;
 
 namespace AppConselhos.Services
 {
-    class DataService
+    public class DataService
     {
         public static async Task<Conselho> GetAdviceRamdom()
         {
             string url = "https://api.adviceslip.com/advice";
             dynamic result = await getDataFromService(url).ConfigureAwait(false);
 
-            if(result["slip"] != null)
+            if (result["slip"] != null)
             {
                 Conselho advices = new Conselho();
                 advices.Id = (string)result["slip"]["id"];
@@ -24,8 +25,9 @@ namespace AppConselhos.Services
                 return advices;
             }
             else
-
-            return null;
+            {
+              return null;
+            }
         }
 
         public static async Task<dynamic> getDataFromService(string url)
@@ -39,8 +41,8 @@ namespace AppConselhos.Services
                 string json = response.Content.ReadAsStringAsync().Result;
                 data = JsonConvert.DeserializeObject(json);
             }
-
-            return data;
+                return data;
+            
         }
 
         public static async Task<dynamic> GetAdviceByNum(string Num)
@@ -56,8 +58,31 @@ namespace AppConselhos.Services
                 return advices;
             }
             else
-
+            { 
                 return null;
+            }
+        }
+
+        public static async Task<dynamic> getDataFromServiceByNum(string url)
+        {
+
+
+
+            Console.WriteLine(url);
+
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            dynamic data = null;
+
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+            return data;
         }
     }
 }
+    
+
--- AppConselhos/AppConselhos/View/PaginaConselho.xaml.cs
+++ AppConselhos/AppConselhos/View/PaginaConselho.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AppConselhos.Model;
+using AppConselhos.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,23 +12,43 @@ using Xamarin.Forms.Xaml;
 
 namespace AppConselhos.View
 {
+    [DesignTimeVisible(false)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PaginaConselho : ContentPage
     {
+        public async void Default()
+        {
+            Conselho advice = await DataService.GetAdviceByNum("69");
+            this.BindingContext = advice;
+        }
         public PaginaConselho()
         {
             InitializeComponent();
+
+            this.BindingContext = new Conselho();
+            Default();
+
+
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
             try
             {
-
+                if (!String.IsNullOrEmpty(ent_Num.Text))
+                {
+                    Conselho advices = await DataService.GetAdviceByNum(ent_Num.Text);
+                    this.BindingContext = advices;
+                }
+                else
+                {
+                    Conselho advices = await DataService.GetAdviceRamdom();
+                    this.BindingContext = advices;
+                }
             }
-            catch
+            catch (Exception ex)
             {
-
+                await DisplayAlert("Erro", ex.Message, "ok");
             }
 
         }
